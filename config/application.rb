6aa1c4d7db690db191db192a35d7f8d5436cdf44
revathiparimi <revathi.parimi@mtuity.com>
2016-09-24 22:15:06 +0530

    app configurations

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,4 @@
+require File.expand_path('../boot', __FILE__)
 require_relative 'boot'
 
 require 'rails/all'
@@ -5,9 +6,14 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+require "devise"
 
 module MyConference
   class Application < Rails::Application
+
+  	config.serve_static_files = true
+    config.api_only = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
